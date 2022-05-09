3857e73775f1f6ddb119165b7694a410d0ac99c9
arfranck <arthur.franck@student.uclouvain.be>
2022-05-09 15:08:07 +0200

    everything in functions

--- part1.py
+++ part1.py
@@ -1,29 +1,17 @@
-# example of loading the cifar10 dataset
+# test harness for evaluating models on the cifar10 dataset
+import sys
+
 from matplotlib import pyplot
-from keras.datasets import cifar10
+from tensorflow.keras.datasets import cifar10
 from tensorflow.keras.utils import to_categorical
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Conv2D
+from tensorflow.keras.layers import MaxPooling2D
+from tensorflow.keras.layers import Dense
+from tensorflow.keras.layers import Flatten
+from tensorflow.keras.optimizers import SGD
 
-# load dataset
-(trainX, trainy), (testX, testy) = cifar10.load_data()
-
-# summarize loaded dataset
-print('Train: X=%s, y=%s' % (trainX.shape, trainy.shape))
-print('Test: X=%s, y=%s' % (testX.shape, testy.shape))
-
-# plot first few images
-for i in range(9):
-	# define subplot
-	pyplot.subplot(330 + 1 + i)
-	# plot raw pixel data
-	pyplot.imshow(trainX[i])
-# show the figure
-pyplot.show()
-
-
-# one hot encode target values
-trainY = to_categorical(trainY)
-testY = to_categorical(testY)
-
+# load train and test dataset
 def load_dataset():
 	# load dataset
 	(trainX, trainY), (testX, testY) = cifar10.load_data()
@@ -32,14 +20,6 @@ def load_dataset():
 	testY = to_categorical(testY)
 	return trainX, trainY, testX, testY
 
-
-
-# convert from integers to floats
-train_norm = train.astype('float32')
-test_norm = test.astype('float32')
-# normalize to range 0-1
-train_norm = train_norm / 255.0
-test_norm = test_norm / 255.0
 
 # scale pixels
 def prep_pixels(train, test):
@@ -51,3 +31,55 @@ def prep_pixels(train, test):
 	test_norm = test_norm / 255.0
 	# return normalized images
 	return train_norm, test_norm
+ 
+
+# define cnn model
+def define_model():
+	model = Sequential()
+	model.add(Conv2D(32, (3, 3), activation='relu', kernel_initializer='he_uniform', padding='same', input_shape=(32, 32, 3)))
+	model.add(Conv2D(32, (3, 3), activation='relu', kernel_initializer='he_uniform', padding='same'))
+	model.add(MaxPooling2D((2, 2)))
+	model.add(Flatten())
+	model.add(Dense(128, activation='relu', kernel_initializer='he_uniform'))
+	model.add(Dense(10, activation='softmax'))
+	# compile model
+	opt = SGD(lr=0.001, momentum=0.9)
+	model.compile(optimizer=opt, loss='categorical_crossentropy', metrics=['accuracy'])
+	return model
+
+
+# plot diagnostic learning curves
+def summarize_diagnostics(history):
+	# plot loss
+	pyplot.subplot(211)
+	pyplot.title('Cross Entropy Loss')
+	pyplot.plot(history.history['loss'], color='blue', label='train')
+	pyplot.plot(history.history['val_loss'], color='orange', label='test')
+	# plot accuracy
+	pyplot.subplot(212)
+	pyplot.title('Classification Accuracy')
+	pyplot.plot(history.history['accuracy'], color='blue', label='train')
+	pyplot.plot(history.history['val_accuracy'], color='orange', label='test')
+	# save plot to file
+	filename = sys.argv[0].split('/')[-1]
+	pyplot.savefig(filename + '_plot.png')
+	pyplot.close()
+ 
+# run the test harness for evaluating a model
+def run_test_harness():
+	# load dataset
+	trainX, trainY, testX, testY = load_dataset()
+	# prepare pixel data
+	trainX, testX = prep_pixels(trainX, testX)
+	# define model
+	model = define_model()
+	# fit model
+	history = model.fit(trainX, trainY, epochs=100, batch_size=64, validation_data=(testX, testY), verbose=0)
+	# evaluate model
+	_, acc = model.evaluate(testX, testY, verbose=0)
+	print('> %.3f' % (acc * 100.0))
+	# learning curves
+	summarize_diagnostics(history)
+ 
+# entry point, run the test harness
+run_test_harness()
