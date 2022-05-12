789123f20f870ffb18c779ce9211190f64963028
giusedyn <84832880+giusedyn@users.noreply.github.com>
2022-05-12 10:53:22 +0200

    Update weight_decay.py

--- weight_decay.py
+++ weight_decay.py
@@ -60,20 +60,20 @@ def define_model():
 
 # plot diagnostic learning curves
 def summarize_diagnostics(history):
-	# plot loss
-	pyplot.subplot(211)
-	pyplot.title('Cross Entropy Loss', fontsize='small')
-	pyplot.plot(history.history['loss'], color='blue', label='train')
-	pyplot.plot(history.history['val_loss'], color='orange', label='test')
-	# plot accuracy
-	pyplot.subplot(212)
-	pyplot.title('Classification Accuracy', fontsize='small')
-	pyplot.plot(history.history['accuracy'], color='blue', label='train')
-	pyplot.plot(history.history['val_accuracy'], color='orange', label='test')
-	# save plot to file
-	filename = sys.argv[0].split('/')[-1]
-	pyplot.savefig(filename + '_plot.png')
-	pyplot.close()
+    fig, (ax1, ax2) = pyplot.subplots(2)
+    # plot loss
+    ax1.set_title('Cross Entropy Loss')
+    ax1.plot(history.history['loss'], color='blue', label='train')
+    ax1.plot(history.history['val_loss'], color='orange', label='test')
+    # plot accuracy
+    ax2.set_title('Classification Accuracy')
+    ax2.plot(history.history['accuracy'], color='blue', label='train')
+    ax2.plot(history.history['val_accuracy'], color='orange', label='test')
+    # save plot to file
+    fig.tight_layout()
+    filename = sys.argv[0].split('/')[-1]
+    fig.savefig(filename + '_plot.png')
+    pyplot.close()
 
 # run the test harness for evaluating a model
 def run_test_harness():
