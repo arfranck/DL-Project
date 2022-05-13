7a61c5ff1943e4b5c67f9a3975a1c89d88d0b1f5
VFAFuzzball <64901331+VFAFuzzball@users.noreply.github.com>
2022-05-13 17:40:23 +0200

    Update cosine_decay_restarts.py

--- cosine_decay_restarts.py
+++ cosine_decay_restarts.py
@@ -21,23 +21,23 @@ from tensorflow.keras.callbacks import LearningRateScheduler
 
 # load train and test dataset
 def load_dataset():
-	# load dataset
-	(trainX, trainY), (testX, testY) = cifar10.load_data()
-	# one hot encode target values
-	trainY = to_categorical(trainY)
-	testY = to_categorical(testY)
-	return trainX, trainY, testX, testY
+    # load dataset
+    (trainX, trainY), (testX, testY) = cifar10.load_data()
+    # one hot encode target values
+    trainY = to_categorical(trainY)
+    testY = to_categorical(testY)
+    return trainX, trainY, testX, testY
 
 # scale pixels
 def prep_pixels(train, test):
-	# convert from integers to floats
-	train_norm = train.astype('float32')
-	test_norm = test.astype('float32')
-	# normalize to range 0-1
-	train_norm = train_norm / 255.0
-	test_norm = test_norm / 255.0
-	# return normalized images
-	return train_norm, test_norm
+    # convert from integers to floats
+    train_norm = train.astype('float32')
+    test_norm = test.astype('float32')
+    # normalize to range 0-1
+    train_norm = train_norm / 255.0
+    test_norm = test_norm / 255.0
+    # return normalized images
+    return train_norm, test_norm
 
 # define cnn model
 def define_model():
