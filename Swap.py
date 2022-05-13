17101da708ba4238b59d86ffe91279dfd0bef579
VFAFuzzball <64901331+VFAFuzzball@users.noreply.github.com>
2022-05-13 17:03:10 +0200

    Add files via upload

---
+++ Swap.py
@@ -0,0 +1,120 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+import sys
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+from matplotlib import pyplot
+from keras.datasets import cifar10
+from tensorflow.keras.utils import to_categorical
+from keras.models import Sequential
+from keras.layers import Conv2D
+from keras.layers import MaxPooling2D
+from keras.layers import Dense
+from keras.layers import Dropout
+from keras.layers import Flatten
+from keras.layers import BatchNormalization
+from tensorflow.keras.optimizers import SGD
+from keras.preprocessing.image import ImageDataGenerator
+
+# load train and test dataset
+def load_dataset():
+	# load dataset
+	(trainX, trainY), (testX, testY) = cifar10.load_data()
+	# one hot encode target values
+	trainY = to_categorical(trainY)
+	testY = to_categorical(testY)
+	return trainX, trainY, testX, testY
+
+# scale pixels
+def prep_pixels(train, test):
+	# convert from integers to floats
+	train_norm = train.astype('float32')
+	test_norm = test.astype('float32')
+	# normalize to range 0-1
+	train_norm = train_norm / 255.0
+	test_norm = test_norm / 255.0
+	# return normalized images
+	return train_norm, test_norm
+
+# define cnn model
+def define_model():
+    model = Sequential()
+    model.add(Conv2D(32, (3, 3), activation='relu', kernel_initializer='he_uniform', padding='same', input_shape=(32, 32, 3)))
+    model.add(BatchNormalization())
+    model.add(Conv2D(32, (3, 3), activation='relu', kernel_initializer='he_uniform', padding='same'))
+    model.add(MaxPooling2D((2, 2)))
+    model.add(Dropout(0.2))
+    model.add(BatchNormalization())
+    model.add(Conv2D(64, (3, 3), activation='relu', kernel_initializer='he_uniform', padding='same'))
+    model.add(BatchNormalization())
+    model.add(Conv2D(64, (3, 3), activation='relu', kernel_initializer='he_uniform', padding='same'))
+    model.add(MaxPooling2D((2, 2)))
+    model.add(Dropout(0.3))
+    model.add(BatchNormalization())
+    model.add(Conv2D(128, (3, 3), activation='relu', kernel_initializer='he_uniform', padding='same'))
+    model.add(BatchNormalization())
+    model.add(Conv2D(128, (3, 3), activation='relu', kernel_initializer='he_uniform', padding='same'))
+    model.add(MaxPooling2D((2, 2)))
+    model.add(Dropout(0.4))
+    model.add(BatchNormalization())
+    model.add(Flatten())
+    model.add(Dense(128, activation='relu', kernel_initializer='he_uniform'))
+    model.add(Dropout(0.5))
+    model.add(BatchNormalization())
+    model.add(Dense(10, activation='softmax'))
+	# compile model
+    opt = SGD(learning_rate=0.001, momentum=0.9)
+    model.compile(optimizer=opt, loss='categorical_crossentropy', metrics=['accuracy'])
+    return model
+
+# plot diagnostic learning curves
+def summarize_diagnostics(history):
+    fig, (ax1, ax2) = pyplot.subplots(2, figsize=(10, 10))
+    # plot loss
+    ax1.set_title('Cross Entropy Loss')
+    ax1.plot(history.history['loss'], color='blue', label='train')
+    ax1.plot(history.history['val_loss'], color='orange', label='test')
+    ax1.set_xlabel('Epochs', fontsize='small')
+    ax1.set_ylabel('Loss', fontsize='small')
+    ax1.legend(loc='best')
+    # plot accuracy
+    ax2.set_title('Classification Accuracy')
+    ax2.plot(history.history['accuracy'], color='blue', label='train')
+    ax2.plot(history.history['val_accuracy'], color='orange', label='test')
+    ax2.set_xlabel('Epochs', fontsize='small')
+    ax2.set_ylabel('Accuracy', fontsize='small')
+    ax2.legend(loc='best')
+    # save plot to file
+    fig.tight_layout()
+    filename = sys.argv[0].split('/')[-1]
+    fig.savefig(filename + '_plot.pdf')
+    pyplot.close()
+
+# run the test harness for evaluating a model
+def run_test_harness():
+	# load dataset
+	trainX, trainY, testX, testY = load_dataset()
+	# prepare pixel data
+	trainX, testX = prep_pixels(trainX, testX)
+	# define model
+	model = define_model()
+	# create data generator
+	datagen = ImageDataGenerator(width_shift_range=0.1, height_shift_range=0.1, horizontal_flip=True)
+	# prepare iterator
+	it_train = datagen.flow(trainX, trainY, batch_size=64)
+	# fit model
+	steps = int(trainX.shape[0] / 64)
+	history = model.fit_generator(it_train, steps_per_epoch=steps, epochs=400, validation_data=(testX, testY), verbose=1)
+	# evaluate model
+	_, acc = model.evaluate(testX, testY, verbose=0)
+	print('> %.3f' % (acc * 100.0))
+	# learning curves
+	summarize_diagnostics(history)
+
+# entry point, run the test harness
+run_test_harness()
