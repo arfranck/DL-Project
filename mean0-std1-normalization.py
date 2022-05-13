9bd983b88c9f881b859fe19106f4374642d40909
VFAFuzzball <64901331+VFAFuzzball@users.noreply.github.com>
2022-05-13 17:36:00 +0200

    Update mean0-std1-normalization.py

--- mean0-std1-normalization.py
+++ mean0-std1-normalization.py
@@ -21,56 +21,56 @@ ssl._create_default_https_context = ssl._create_unverified_context
 
 # load train and test dataset
 def load_dataset():
-	# load dataset
-	(trainX, trainY), (testX, testY) = cifar10.load_data()
-	# one hot encode target values
-	trainY = to_categorical(trainY)
-	testY = to_categorical(testY)
-	return trainX, trainY, testX, testY
+    # load dataset
+    (trainX, trainY), (testX, testY) = cifar10.load_data()
+    # one hot encode target values
+    trainY = to_categorical(trainY)
+    testY = to_categorical(testY)
+    return trainX, trainY, testX, testY
 
 # scale pixels
 def prep_pixels(train, test):
-	# convert from integers to floats
-	train_norm = train.astype('float32')
-	test_norm = test.astype('float32')
-	# normalize to mean 0 std 1
-	mean = train_norm.mean(axis=0)
-	std = train_norm.std(axis=0)
-	train_norm = (train_norm - mean) / std 
-	test_norm = (test_norm - mean) / std 
-	# return normalized images
-	return train_norm, test_norm
+    # convert from integers to floats
+    train_norm = train.astype('float32')
+    test_norm = test.astype('float32')
+    # normalize to mean 0 std 1
+    mean = train_norm.mean(axis=0)
+    std = train_norm.std(axis=0)
+    train_norm = (train_norm - mean) / std 
+    test_norm = (test_norm - mean) / std 
+    # return normalized images
+    return train_norm, test_norm
 
 # define cnn model
 def define_model():
-	model = Sequential()
-	model.add(Conv2D(32, (3, 3), activation='relu', kernel_initializer='he_uniform', padding='same', input_shape=(32, 32, 3)))
-	model.add(BatchNormalization())
-	model.add(Conv2D(32, (3, 3), activation='relu', kernel_initializer='he_uniform', padding='same'))
-	model.add(BatchNormalization())
-	model.add(MaxPooling2D((2, 2)))
-	model.add(Dropout(0.2))
-	model.add(Conv2D(64, (3, 3), activation='relu', kernel_initializer='he_uniform', padding='same'))
-	model.add(BatchNormalization())
-	model.add(Conv2D(64, (3, 3), activation='relu', kernel_initializer='he_uniform', padding='same'))
-	model.add(BatchNormalization())
-	model.add(MaxPooling2D((2, 2)))
-	model.add(Dropout(0.3))
-	model.add(Conv2D(128, (3, 3), activation='relu', kernel_initializer='he_uniform', padding='same'))
-	model.add(BatchNormalization())
-	model.add(Conv2D(128, (3, 3), activation='relu', kernel_initializer='he_uniform', padding='same'))
-	model.add(BatchNormalization())
-	model.add(MaxPooling2D((2, 2)))
-	model.add(Dropout(0.4))
-	model.add(Flatten())
-	model.add(Dense(128, activation='relu', kernel_initializer='he_uniform'))
-	model.add(BatchNormalization())
-	model.add(Dropout(0.5))
-	model.add(Dense(10, activation='softmax'))
-	# compile model
-	opt = SGD(lr=0.001, momentum=0.9)
-	model.compile(optimizer=opt, loss='categorical_crossentropy', metrics=['accuracy'])
-	return model
+    model = Sequential()
+    model.add(Conv2D(32, (3, 3), activation='relu', kernel_initializer='he_uniform', padding='same', input_shape=(32, 32, 3)))
+    model.add(BatchNormalization())
+    model.add(Conv2D(32, (3, 3), activation='relu', kernel_initializer='he_uniform', padding='same'))
+    model.add(BatchNormalization())
+    model.add(MaxPooling2D((2, 2)))
+    model.add(Dropout(0.2))
+    model.add(Conv2D(64, (3, 3), activation='relu', kernel_initializer='he_uniform', padding='same'))
+    model.add(BatchNormalization())
+    model.add(Conv2D(64, (3, 3), activation='relu', kernel_initializer='he_uniform', padding='same'))
+    model.add(BatchNormalization())
+    model.add(MaxPooling2D((2, 2)))
+    model.add(Dropout(0.3))
+    model.add(Conv2D(128, (3, 3), activation='relu', kernel_initializer='he_uniform', padding='same'))
+    model.add(BatchNormalization())
+    model.add(Conv2D(128, (3, 3), activation='relu', kernel_initializer='he_uniform', padding='same'))
+    model.add(BatchNormalization())
+    model.add(MaxPooling2D((2, 2)))
+    model.add(Dropout(0.4))
+    model.add(Flatten())
+    model.add(Dense(128, activation='relu', kernel_initializer='he_uniform'))
+    model.add(BatchNormalization())
+    model.add(Dropout(0.5))
+    model.add(Dense(10, activation='softmax'))
+    # compile model
+    opt = SGD(lr=0.001, momentum=0.9)
+    model.compile(optimizer=opt, loss='categorical_crossentropy', metrics=['accuracy'])
+    return model
 
 # plot diagnostic learning curves
 def summarize_diagnostics(history):
@@ -97,25 +97,25 @@ def summarize_diagnostics(history):
 
 # run the test harness for evaluating a model
 def run_test_harness(epochs=400):
-	# load dataset
-	trainX, trainY, testX, testY = load_dataset()
-	# prepare pixel data
-	trainX, testX = prep_pixels(trainX, testX)
-	# define model
-	model = define_model()
-	# create data generator
-	datagen = ImageDataGenerator(width_shift_range=0.1, height_shift_range=0.1, horizontal_flip=True)
-	# prepare iterator
-	it_train = datagen.flow(trainX, trainY, batch_size=64)
-	# fit model
-	steps = int(trainX.shape[0] / 64)
-	history = model.fit_generator(it_train, steps_per_epoch=steps, epochs=epochs, validation_data=(testX, testY), verbose=1)
-	# evaluate model
-	_, acc = model.evaluate(testX, testY, verbose=0)
-	print('> %.3f' % (acc * 100.0))
-	# learning curves
-	summarize_diagnostics(history)
-	return model, history
+    # load dataset
+    trainX, trainY, testX, testY = load_dataset()
+    # prepare pixel data
+    trainX, testX = prep_pixels(trainX, testX)
+    # define model
+    model = define_model()
+    # create data generator
+    datagen = ImageDataGenerator(width_shift_range=0.1, height_shift_range=0.1, horizontal_flip=True)
+    # prepare iterator
+    it_train = datagen.flow(trainX, trainY, batch_size=64)
+    # fit model
+    steps = int(trainX.shape[0] / 64)
+    history = model.fit_generator(it_train, steps_per_epoch=steps, epochs=epochs, validation_data=(testX, testY), verbose=1)
+    # evaluate model
+    _, acc = model.evaluate(testX, testY, verbose=0)
+    print('> %.3f' % (acc * 100.0))
+    # learning curves
+    summarize_diagnostics(history)
+    return model, history
 
 # entry point, run the test harness
-model, history = run_test_harness()
+run_test_harness()
