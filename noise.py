f981b4e9b48eaa2954716c33d609a9216dffdd14
VFAFuzzball <64901331+VFAFuzzball@users.noreply.github.com>
2022-05-15 17:26:50 +0200

    Update noise.py

--- noise.py
+++ noise.py
@@ -1,70 +1,145 @@
-# disturb the CIFAR-100 with the specific noise
+#---------------------------------------------------------------------
+#----------- DD2424 Deep Learning in Data Science Project ------------
+#----------- Cataldo Giuseppe, Franck Arthur, Nameki Malo ------------
+#---------------------------------------------------------------------
+
+import sys
+from matplotlib import pyplot
+from keras.datasets import cifar10
+from tensorflow.keras.utils import to_categorical
+from keras.models import Sequential
+from keras.layers import Conv2D
+from keras.layers import MaxPooling2D
+from keras.layers import Dense
+from keras.layers import Flatten
+from tensorflow.keras.optimizers import Adam
+from keras.preprocessing.image import ImageDataGenerator
+from keras.layers import Dropout
+from keras.layers import BatchNormalization
 import numpy as np
+import random
+import copy
 
-# binary format
-# <1 byte for coarse label> <1 byte for fine label> <3072 byte for pixels>
+def add_noise(trainY):
+    noise = 10
+    random_idx = []
+    to_change = len(trainY) * noise/100
+    changed = 0
+    idx = {}
+    while changed < to_change:
+        index = random.randint(0,len(trainY))
+        if index in idx:
+            continue
+        new_label = random.randint(0, 10)
+        while new_label == trainY[index]:
+            new_label = random.randint(0, 10)
+        trainY[index] = new_label
+        changed += 1
+        idx[index] = True
+    return trainY
 
-def gen_noise_tridiagonal():
-  # Three diagonals
-  NOISY_PROPORTION = 0.6
-  T = np.zeros((10,10))
-  T[0][0],T[0][1] = 1.0 - NOISY_PROPORTION + 0.2, NOISY_PROPORTION - 0.2
-  T[1][0],T[1][1],T[1][2] = NOISY_PROPORTION/2.0, 1.0 - NOISY_PROPORTION, NOISY_PROPORTION/2.0
-  T[2][1],T[2][2],T[2][3] = NOISY_PROPORTION/2.0, 1.0 - NOISY_PROPORTION, NOISY_PROPORTION/2.0
-  T[3][2],T[3][3],T[3][4] = NOISY_PROPORTION/2.0, 1.0 - NOISY_PROPORTION, NOISY_PROPORTION/2.0
-  T[4][3],T[4][4],T[4][5] = NOISY_PROPORTION/2.0, 1.0 - NOISY_PROPORTION, NOISY_PROPORTION/2.0
-  T[5][4],T[5][5],T[5][6] = NOISY_PROPORTION/2.0, 1.0 - NOISY_PROPORTION, NOISY_PROPORTION/2.0
-  T[6][5],T[6][6],T[6][7] = NOISY_PROPORTION/2.0, 1.0 - NOISY_PROPORTION, NOISY_PROPORTION/2.0
-  T[7][6],T[7][7],T[7][8] = NOISY_PROPORTION/2.0, 1.0 - NOISY_PROPORTION, NOISY_PROPORTION/2.0
-  T[8][7],T[8][8],T[8][9] = NOISY_PROPORTION/2.0, 1.0 - NOISY_PROPORTION, NOISY_PROPORTION/2.0
-  T[9][8],T[9][9] = NOISY_PROPORTION - 0.2, 1.0 - NOISY_PROPORTION + 0.2
+# load train and test dataset
+def load_dataset():
+    # load dataset
+    (trainX, trainY), (testX, testY) = cifar10.load_data()
+    old_trainY = trainY
+    trainY = add_noise(copy.deepcopy(trainY))
+    count = 0
+    for i in range(len(trainY)):
+        if trainY[i] != old_trainY[i]:
+            count += 1
+    print(count)
+    # one hot encode target values
+    trainY = to_categorical(trainY)
+    testY = to_categorical(testY)
+    return trainX, trainY, testX, testY
 
-  return T
+# scale pixels
+def prep_pixels(train, test):
+    # convert from integers to floats
+    train_norm = train.astype('float32')
+    test_norm = test.astype('float32')
+    # normalize to range 0-1
+    train_norm = train_norm / 255.0
+    test_norm = test_norm / 255.0
+    # return normalized images
+    return train_norm, test_norm
 
-def gen_noise_column():
-  # Column disturbs
-  NOISY_PROPORTION = 0.6
-  T = np.zeros((10,10))
-  T[0][0],T[0][3],T[0][5] = 1.0 - NOISY_PROPORTION, NOISY_PROPORTION/2.0, NOISY_PROPORTION/2.0
-  T[1][1],T[1][3],T[1][5] = 1.0 - NOISY_PROPORTION, NOISY_PROPORTION/2.0, NOISY_PROPORTION/2.0
-  T[2][2],T[2][3],T[2][5] = 1.0 - NOISY_PROPORTION, NOISY_PROPORTION/2.0, NOISY_PROPORTION/2.0
-  T[3][3],T[3][5] = 1.0 - NOISY_PROPORTION + 0.2, NOISY_PROPORTION - 0.2
-  T[4][4],T[4][3],T[4][5] = 1.0 - NOISY_PROPORTION, NOISY_PROPORTION/2.0, NOISY_PROPORTION/2.0
-  T[5][5],T[5][3] = 1.0 - NOISY_PROPORTION + 0.2, NOISY_PROPORTION - 0.2
-  T[6][6],T[6][3],T[6][5] = 1.0 - NOISY_PROPORTION, NOISY_PROPORTION/2.0, NOISY_PROPORTION/2.0
-  T[7][7],T[7][3],T[7][5] = 1.0 - NOISY_PROPORTION, NOISY_PROPORTION/2.0, NOISY_PROPORTION/2.0
-  T[8][8],T[8][3],T[8][5] = 1.0 - NOISY_PROPORTION, NOISY_PROPORTION/2.0, NOISY_PROPORTION/2.0
-  T[9][9],T[9][3],T[9][5] = 1.0 - NOISY_PROPORTION, NOISY_PROPORTION/2.0, NOISY_PROPORTION/2.0
+# define cnn model
+def define_model():
+    model = Sequential()
+    model.add(Conv2D(32, (3, 3), activation='relu', kernel_initializer='he_uniform', padding='same', input_shape=(32, 32, 3)))
+    model.add(BatchNormalization())
+    model.add(Conv2D(32, (3, 3), activation='relu', kernel_initializer='he_uniform', padding='same'))
+    model.add(BatchNormalization())
+    model.add(MaxPooling2D((2, 2)))
+    model.add(Dropout(0.2))
+    model.add(Conv2D(64, (3, 3), activation='relu', kernel_initializer='he_uniform', padding='same'))
+    model.add(BatchNormalization())
+    model.add(Conv2D(64, (3, 3), activation='relu', kernel_initializer='he_uniform', padding='same'))
+    model.add(BatchNormalization())
+    model.add(MaxPooling2D((2, 2)))
+    model.add(Dropout(0.3))
+    model.add(Conv2D(128, (3, 3), activation='relu', kernel_initializer='he_uniform', padding='same'))
+    model.add(BatchNormalization())
+    model.add(Conv2D(128, (3, 3), activation='relu', kernel_initializer='he_uniform', padding='same'))
+    model.add(BatchNormalization())
+    model.add(MaxPooling2D((2, 2)))
+    model.add(Dropout(0.4))
+    model.add(Flatten())
+    model.add(Dense(128, activation='relu', kernel_initializer='he_uniform'))
+    model.add(BatchNormalization())
+    model.add(Dropout(0.5))
+    model.add(Dense(10, activation='softmax'))
+    # compile model
+    opt = Adam(learning_rate=0.001)
+    model.compile(optimizer=opt, loss='categorical_crossentropy', metrics=['accuracy'])
+    return model
 
-  return T
+# plot diagnostic learning curves
+def summarize_diagnostics(history):
+    fig, (ax1, ax2) = pyplot.subplots(2, figsize=(10, 10))
+    # plot loss
+    ax1.set_title('Cross Entropy Loss')
+    ax1.plot(history.history['loss'], color='blue', label='train')
+    ax1.plot(history.history['val_loss'], color='orange', label='test')
+    ax1.set_xlabel('Epochs', fontsize='small')
+    ax1.set_ylabel('Loss', fontsize='small')
+    ax1.legend(loc='best')
+    # plot accuracy
+    ax2.set_title('Classification Accuracy')
+    ax2.plot(history.history['accuracy'], color='blue', label='train')
+    ax2.plot(history.history['val_accuracy'], color='orange', label='test')
+    ax2.set_xlabel('Epochs', fontsize='small')
+    ax2.set_ylabel('Accuracy', fontsize='small')
+    ax2.legend(loc='best')
+    # save plot to file
+    fig.tight_layout()
+    filename = sys.argv[0].split('/')[-1]
+    fig.savefig(filename + '_plot.pdf')
+    pyplot.close()
 
-def dis_noise(label,T):
-   return xrange(10)[np.argmax(np.random.multinomial(size=1,n=1,pvals=T[label]))]
+# run the test harness for evaluating a model
+def run_test_harness():
+    # load dataset
+    trainX, trainY, testX, testY = load_dataset()
+    # prepare pixel data
+    #trainX, testX = prep_pixels(trainX, testX)
+    # define model
+    #model = define_model()
+    # create data generator
+    #datagen = ImageDataGenerator(width_shift_range=0.1, height_shift_range=0.1, horizontal_flip=True)
+    # prepare iterator
+    #it_train = datagen.flow(trainX, trainY, batch_size=64)
+    # fit model
+    #steps = int(trainX.shape[0] / 64)
+    #history = model.fit(it_train, steps_per_epoch=steps, epochs=1, validation_data=(testX, testY), verbose=1)
+    # evaluate model
+    #_, acc = model.evaluate(testX, testY, verbose=0)
+    #print('> %.3f' % (acc * 100.0))
+    # learning curves
+    #summarize_diagnostics(history)
+    return trainX, trainY, testX, testY
 
-def inject_data(noise, filename, noise_name):
-   with open(filename,'rb') as f:
-      with open(filename[:-4] + '_' + noise_name  + filename[-4:],'wb') as w:
-        e = f.read(3073)
-        while e:
-          label = ord(e[0])
-          #print(label)
-          dis_label = dis_noise(label,noise)
-          dis_e = chr(dis_label) + e[1:]
-          w.write(dis_e)
-          e = f.read(3073)
-
-
-
-tridiagonal_noise = gen_noise_tridiagonal()
-inject_data(tridiagonal_noise,'data/cifar10/cifar-10-batches-bin/data_batch_1.bin','tridiagonal')
-inject_data(tridiagonal_noise,'data/cifar10/cifar-10-batches-bin/data_batch_2.bin','tridiagonal')
-inject_data(tridiagonal_noise,'data/cifar10/cifar-10-batches-bin/data_batch_3.bin','tridiagonal')
-inject_data(tridiagonal_noise,'data/cifar10/cifar-10-batches-bin/data_batch_4.bin','tridiagonal')
-inject_data(tridiagonal_noise,'data/cifar10/cifar-10-batches-bin/data_batch_5.bin','tridiagonal')
-
-column_noise = gen_noise_column()
-inject_data(column_noise, 'data/cifar10/cifar-10-batches-bin/data_batch_1.bin','column')
-inject_data(column_noise, 'data/cifar10/cifar-10-batches-bin/data_batch_2.bin','column')
-inject_data(column_noise, 'data/cifar10/cifar-10-batches-bin/data_batch_3.bin','column')
-inject_data(column_noise, 'data/cifar10/cifar-10-batches-bin/data_batch_4.bin','column')
-inject_data(column_noise, 'data/cifar10/cifar-10-batches-bin/data_batch_5.bin','column')
+# entry point, run the test harness
+trainX, trainY, testX, testY = run_test_harness()
